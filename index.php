b88210f020a93302113b7fbe00f97d5b9f34d0af
Stevanus Hia <stevanushiaa@gmail.com>
2024-02-18 12:07:03 +0700

    changing color

--- index.php
+++ index.php
@@ -1,47 +1,17 @@
-<!DOCTYPE html>
-<html>
+<?php include_once __DIR__."/template/header.php"; ?>
 
-<head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
 
-  <title>Goid</title>
-  <!-- slider stylesheet -->
-  <link rel="stylesheet" type="text/css"
-    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Raleway:400,700&display=swap"
-    rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
-</head>
-
-<body>
   <div class="hero_area">
     <!-- header section strats -->
+    
     <header class="header_section">
       <div class="container-fluid">
         <nav class="navbar navbar-expand-lg custom_nav-container">
-          <a class="navbar-brand" href="index.php">
-            <img src="images/logo.png" alt=""/>
-            <span>
-              Goid
-            </span>
+          
+          <a class="" href="index.php">
+            <img src="images/logo.png" alt="" height="250px" width="250px" style="margin-top: -55px; margin-left: -18px;"/>
           </a>
+          
 
           <div class="navbar-collapse" id="">
             <div class="custom_menu-btn">
@@ -68,14 +38,7 @@
     <section class=" slider_section position-relative">
       <div class="side_heading">
         <h5>
-          G
-          o
-          o
-          d
-          F
-          o
-          o
-          d
+          Laoban Nusantara
         </h5>
       </div>
       <div class="container-fluid">
@@ -131,8 +94,8 @@
           <div class=" col-md-5 offset-md-1">
             <div class="detail-box">
               <h1>
-                Good <br>
-                Food
+                Laoban <br>
+                Nusantara
               </h1>
               <p>
                 There are many variations of passages of Lorem Ipsum available, but the majority have suffered
@@ -458,7 +421,7 @@
 
   <!-- contact section -->
 
-  <section class="contact_section layout_padding-bottom ">
+  <!-- <section class="contact_section layout_padding-bottom ">
     <div class="container">
       <h2>
         Get In touch
@@ -507,7 +470,7 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
   <!-- end contact section -->
 
