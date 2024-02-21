4bd7c67107429541660745a611019d1332561e8e
stevenleo11 <stevenleonardi03@gmail.com>
2024-02-21 21:24:34 +0700

    ganti paragraf aboutus

--- index.php
+++ index.php
@@ -54,12 +54,20 @@ include_once __DIR__."/template/header.php";
                             <h6>About Us</h6>
                             <h2>We Leave A Delicious Memory For You</h2>
                         </div>
-                        <p>Klassy Cafe is one of the best <a href="https://templatemo.com/tag/restaurant"
-                                target="_blank" rel="sponsored">restaurant HTML templates</a> with Bootstrap v4.5.2 CSS
-                            framework. You can download and feel free to use this website template layout for your
-                            restaurant business. You are allowed to use this template for commercial purposes.
-                            <br><br>You are NOT allowed to redistribute the template ZIP file on any template donwnload
-                            website. Please contact us for more information.</p>
+                        <p>Berawal dari kegelisahan sang owner dan istri yang mencari rasa otentik kopitiam untuk warga lokal, LAO BAN terlahir untuk mengungkap bakat masak terpendam sang owner. 
+                            Melalui survei dari Singapura hingga Malaysia, kami menyempurnakan resep makanan, mengolah bumbu non-asli Indonesia tanpa campuran non-halal tentu menjadi tantangan untuk owner LAO BAN dalam perjalanan mencari jati diri.</p> 
+                            
+                        <p>mengolah kelapa hingga menjadi selai kaya, hingga kini masih menjadi andalan para laoban menjadi titik klimaks dalam pencarian jati diri LAO BAN.</p> 
+                            
+                        <p>hingga pada akhirnya penentuan tgl diputuskan, waktu yg tidak terasa habis untuk merekrut para crew dan tepat pada tgl 20 training pertama bersama 6 crew "babat alas" pun dimulai 3 hari berlalu di hari ke 4 kita memutuskan untuk membuka LAO BAN untuk pertama kali nya. 
+                            hari demi hari tidak terasa adrenalin para crew dan owner semakin terguncang bertubi tubi karena derasnya antusias para predator makanan asia.</p> 
+                            
+                        <p>hujatan, pujian, review buruk, cust langganan sampai hatters bayaran semua menjadikan cambuk untuk LAO BAN terpacu jadi semakin besar dan dikenal banyak orang.
+                            sebagai pemanfaat situasi dan pembidik jitu tidak afdol jika LAO BAN mecampakkan platform ig dan tiktok.</p> 
+                            
+                        <p>perjalanan kisah jatuh bangun warga tiktok yg fyp menjadi kiblat LAO BAN untuk membuat cuplikan cerita singkat proses pendakian kedai ini. semakin fyp semakin banyak orang kepo dan fomo terhadap LAO BAN dan yg akhirnya bisa membuat LAO BAN buka cabang di kota kota besar lainnya.</p>
+                            
+                        <p>SUDAH SIAP MENCICIP MENU BARU by Uncle Osh, LAOBAN??</p>
                         <div class="row">
                             <div class="col-4">
                                 <img src="assets/images/about-thumb-01.jpg" alt="">
