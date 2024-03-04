c549b70095abbb31e004feba438b256a7ae1a56e
Stevanus Hia <stevanushiaa@gmail.com>
2024-03-04 23:52:32 +0700

    moving founder from index to about

--- about.php
+++ about.php
@@ -54,5 +54,46 @@ include_once __DIR__."/template/header.php";
 </section>
 <!-- ***** About Area Ends ***** -->
 
+    <!-- ***** Founder Area Starts ***** -->
+    <section class="section" id="about">
+        <div id="chefs">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-xs-12">
+                    <div class="chef-item">
+                            <div class="thumb">
+                                <div class="overlay"></div>
+                                <ul class="social-icons">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                                </ul>
+                                <img src="assets/images/FotoCEO.jpg" alt="Founder" style="width: 325px;" >
+                            </div>
+                            <div class="down-content">
+                                <h4>Alvin Osh</h4>
+                                <span>CEO</span>
+                            </div>
+                        </div>
+                    </div>
+                <div class="col-lg-6 col-md-6 col-xs-12">
+                    <div class="right-content">
+                        <div class="left-text-content">
+                            <div class="section-heading">
+                                <h6>Founder Laoban Nusantara</h6>
+                                <h2 style="margin-bottom: -45px;">ALVIN OSH</h2>
+                            </div>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus maxime, repudiandae facere ad exercitationem ea laborum nostrum libero praesentium autem expedita minima iusto atque in commodi! Incidunt nisi inventore praesentium?
+                            Sequi, architecto in excepturi et, doloremque iste, consectetur vitae tempora sit hic dicta illum qui. Ratione enim nisi delectus atque modi reprehenderit aspernatur ducimus, ut vero, quod omnis excepturi! Tempora!
+                            At vitae rem minus aut quasi placeat praesentium. Quasi fugiat recusandae aliquid ipsam, fugit molestiae provident vel soluta unde ratione incidunt dolores esse repellendus sapiente optio, nemo eum eaque vitae.
+                            </p> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- ***** Founder Area Ends ***** -->
 
 <?php include_once __DIR__."/template/footer.php"; ?>
