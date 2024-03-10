68262f0908cd3d48355a02af64e6a46e7ea60470
stevenleo11 <stevenleonardi03@gmail.com>
2024-03-10 17:32:54 +0700

    a

--- DB/pdo.php
+++ DB/pdo.php
@@ -12,10 +12,10 @@ class DB {
     }
 
     private static function connectDatabase(){
-        $host = "localhost";
-        $db = "laoban_nusantara";
-        $user = "root";
-        $pass = "";
+        $host = "srv1319.hstgr.io";
+        $db = "u540028336_laobandb";
+        $user = "u540028336_laoban";
+        $pass = "Laoban_Nusantara123";
         $options = [
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
