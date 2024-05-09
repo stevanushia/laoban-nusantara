150a8bf2c70fd4cf75d492f5a3d931e673d60431
stevenleo11 <stevenleonardi03@gmail.com>
2024-05-09 18:00:38 +0700

    Outlet

--- ourFranchise.php
+++ ourFranchise.php
@@ -126,7 +126,7 @@ include_once __DIR__."/template/header.php"; ?>
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                        <strong>Laoban Kopitiam Malang (Pusat)</strong>
+                        <strong>Laoban Kopitiam Suhat, Malang (Pusat)</strong>
                     </button>
                 </h2>
                 <div id="collapseFour" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -137,9 +137,22 @@ include_once __DIR__."/template/header.php"; ?>
             </div>
             <div class="accordion-item">
                 <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwelve"
+                        aria-expanded="true" aria-controls="collapseTwelve">
+                        <strong>Laoban Kopitiam Binus, Malang</strong>
+                    </button>
+                </h2>
+                <div id="collapseTwelve" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.551008432692!2d112.68130327433185!3d-7.94187009208232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd62943bed1ea39%3A0x4481cf517a9ea1c9!2sLaoban%20Kopitiam%20Binus!5e0!3m2!1sid!2sid!4v1715252212941!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
                         aria-expanded="true" aria-controls="collapseOne">
-                        <strong>Laoban Kopitiam Wiyung</strong>
+                        <strong>Laoban Kopitiam Wiyung, Surabaya</strong>
                     </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -152,7 +165,7 @@ include_once __DIR__."/template/header.php"; ?>
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        <strong>Laoban Kopitiam Mulyosari</strong>
+                        <strong>Laoban Kopitiam Mulyosari, Surabaya</strong>
                     </button>
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -161,6 +174,21 @@ include_once __DIR__."/template/header.php"; ?>
                     </div>
                 </div>
             </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                        <strong>Laoban Kopitiam Baratajaya, Surabaya</strong>
+                    </button>
+                </h2>
+                <div id="collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63318.75355803201!2d112.67742438081147!3d-7.306385131453962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fbdab3777fc9%3A0x6a5a755769688dbb!2sLaoban%20BarataJaya!5e0!3m2!1sid!2sid!4v1715251413440!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
             <div class="accordion-item">
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -175,7 +203,91 @@ include_once __DIR__."/template/header.php"; ?>
                 </div>
             </div>
 
-            
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
+                        <strong>Laoban Kopitiam Makassar</strong>
+                    </button>
+                </h2>
+                <div id="collapsefive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.796060437951!2d119.40242207430354!3d-5.136513094840663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbef9ed25af80b3%3A0x5c8593f958b12a25!2sLaoban%20Kopitiam%20Makassar!5e0!3m2!1sid!2sid!4v1715250684171!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsesix" aria-expanded="false" aria-controls="collapsesix">
+                        <strong>Laoban Kopitiam Tulungagung</strong>
+                    </button>
+                </h2>
+                <div id="collapsesix" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.449416871812!2d111.91348857433319!3d-8.055555391971941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78e346f2542d6d%3A0xaacbffb4b29c9a0b!2sLaoban%20Tulungagung!5e0!3m2!1sid!2sid!4v1715251077725!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseseven" aria-expanded="false" aria-controls="collapseseven">
+                        <strong>Laoban Kopitiam Mojokerto</strong>
+                    </button>
+                </h2>
+                <div id="collapseseven" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d506015.55117786763!2d111.84551617518312!3d-7.763663348724788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e780d128f10cdc3%3A0x378a7ed7b3ae7005!2sLaoban%20Mojokerto!5e0!3m2!1sid!2sid!4v1715251346395!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                        <strong>Laoban Kopitiam Probolinggo</strong>
+                    </button>
+                </h2>
+                <div id="collapseNine" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.3470045633085!2d113.21211397447304!3d-7.752971172324123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7ad27bdaf4931%3A0x2e1951e8958401fa!2sLaoban%20Kopitiam%20Probolinggo!5e0!3m2!1sid!2sid!4v1715251644975!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+                        <strong>Laoban Kopitiam Jember</strong>
+                    </button>
+                </h2>
+                <div id="collapseTen" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d505765.19808796817!2d113.11238892118087!3d-7.968888878763804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd691f5fb4671bb%3A0x12a6775d7b0caf73!2sLaoban%20Jember!5e0!3m2!1sid!2sid!4v1715251717665!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">
+                        <strong>Laoban Kopitiam Bandung</strong>
+                    </button>
+                </h2>
+                <div id="collapseEleven" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4055782.43437704!2d102.72141021249998!3d-6.9234366000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e7002fefc4cd%3A0x11fec2012f28e0ea!2sLaoban%20by%20Uncle%20Osh%20Bandung!5e0!3m2!1sid!2sid!4v1715251964321!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
         <!-- ACCORDION BOOTSTRAP -->
     </div>
