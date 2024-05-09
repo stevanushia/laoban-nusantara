057b9fc375b4e8d5c2f8685e49ac99ebb51ea0c1
Stevanus Hia <stevanushiaa@gmail.com>
2024-05-09 17:25:37 +0700

    accordion franchise using bootstrap 5

--- ourFranchise.php
+++ ourFranchise.php
@@ -2,120 +2,189 @@
 $current_page = "franchise";
 include_once __DIR__."/template/header.php"; ?>
 
-    <!-- ***** Franchise Area Starts ***** -->
-    <section class="section" id="menu">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="section-heading">
-                        <h6>Our Franchise</h6>
-                        <h2>Our selection of cakes with quality taste</h2>
-                    </div>
+<!-- ***** Franchise Area Starts ***** -->
+<section class="section" id="menu">
+    <!-- <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="section-heading">
+                    <h6>Our Franchise</h6>
+                    <h2>Our selection of cakes with quality taste</h2>
                 </div>
             </div>
         </div>
-        <div class="menu-item-carousel">
-            <div class="col-lg-12">
-                <div class="owl-menu-item owl-carousel">
-                    <div class="item">
-                        <div class='card card1'>
-                            <div class="price">
-                                <h6>$14</h6>
-                            </div>
-                            <div class='info'>
-                                <h1 class='title'>Chocolate Cake</h1>
-                                <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
-                                    eiusmod teme.</p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                                class="fa fa-angle-down"></i></a></div>
-                                </div>
-                            </div>
+    </div>
+    <div class="menu-item-carousel">
+        <div class="col-lg-12">
+            <div class="owl-menu-item owl-carousel">
+                <div class="item">
+                    <div class='card card1'>
+                        <div class="price">
+                            <h6>$14</h6>
                         </div>
-                    </div>
-                    <div class="item">
-                        <div class='card card2'>
-                            <div class="price">
-                                <h6>$22</h6>
-                            </div>
-                            <div class='info'>
-                                <h1 class='title'>Klassy Pancake</h1>
-                                <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
-                                    eiusmod teme.</p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                                class="fa fa-angle-down"></i></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class='card card3'>
-                            <div class="price">
-                                <h6>$18</h6>
-                            </div>
-                            <div class='info'>
-                                <h1 class='title'>Tall Klassy Bread</h1>
-                                <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
-                                    eiusmod teme.</p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                                class="fa fa-angle-down"></i></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class='card card4'>
-                            <div class="price">
-                                <h6>$10</h6>
-                            </div>
-                            <div class='info'>
-                                <h1 class='title'>Blueberry CheeseCake</h1>
-                                <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
-                                    eiusmod teme.</p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                                class="fa fa-angle-down"></i></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class='card card5'>
-                            <div class="price">
-                                <h6>$8.50</h6>
-                            </div>
-                            <div class='info'>
-                                <h1 class='title'>Klassy Cup Cake</h1>
-                                <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
-                                    eiusmod teme.</p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                                class="fa fa-angle-down"></i></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class='card card3'>
-                            <div class="price">
-                                <h6>$7.25</h6>
-                            </div>
-                            <div class='info'>
-                                <h1 class='title'>Klassic Cake</h1>
-                                <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
-                                    eiusmod teme.</p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
-                                                class="fa fa-angle-down"></i></a></div>
-                                </div>
+                        <div class='info'>
+                            <h1 class='title'>Chocolate Cake</h1>
+                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
+                                eiusmod teme.</p>
+                            <div class="main-text-button">
+                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
+                                            class="fa fa-angle-down"></i></a></div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="item">
+                    <div class='card card2'>
+                        <div class="price">
+                            <h6>$22</h6>
+                        </div>
+                        <div class='info'>
+                            <h1 class='title'>Klassy Pancake</h1>
+                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
+                                eiusmod teme.</p>
+                            <div class="main-text-button">
+                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
+                                            class="fa fa-angle-down"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class='card card3'>
+                        <div class="price">
+                            <h6>$18</h6>
+                        </div>
+                        <div class='info'>
+                            <h1 class='title'>Tall Klassy Bread</h1>
+                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
+                                eiusmod teme.</p>
+                            <div class="main-text-button">
+                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
+                                            class="fa fa-angle-down"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class='card card4'>
+                        <div class="price">
+                            <h6>$10</h6>
+                        </div>
+                        <div class='info'>
+                            <h1 class='title'>Blueberry CheeseCake</h1>
+                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
+                                eiusmod teme.</p>
+                            <div class="main-text-button">
+                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
+                                            class="fa fa-angle-down"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class='card card5'>
+                        <div class="price">
+                            <h6>$8.50</h6>
+                        </div>
+                        <div class='info'>
+                            <h1 class='title'>Klassy Cup Cake</h1>
+                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
+                                eiusmod teme.</p>
+                            <div class="main-text-button">
+                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
+                                            class="fa fa-angle-down"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class='card card3'>
+                        <div class="price">
+                            <h6>$7.25</h6>
+                        </div>
+                        <div class='info'>
+                            <h1 class='title'>Klassic Cake</h1>
+                            <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do
+                                eiusmod teme.</p>
+                            <div class="main-text-button">
+                                <div class="scroll-to-section"><a href="#reservation">Make Reservation <i
+                                            class="fa fa-angle-down"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </section>
-    <!-- ***** Franchise Area Ends ***** -->
+    </div>
+    <br><br><br><br><br><br><br><br>
+    <br><br><br><br> -->
+
+    <div class="col-6 col-md-4">
+        <!-- ACCORDION BOOTSTRAP -->
+        <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <strong>Laoban Kopitiam Malang (Pusat)</strong>
+                    </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d126449.96895010203!2d112.54751546940913!3d-7.940776492494127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x2dd6298ddfd73cf5%3A0xbeb5ae954f915c2c!2sJl.%20Soekarno%20Hatta%20Indah%20No.17%2C%20Mojolangu%2C%20Kec.%20Lowokwaru%2C%20Kota%20Malang%2C%20Jawa%20Timur%2065142!3m2!1d-7.9407812!2d112.6299076!5e0!3m2!1sen!2sid!4v1715249968798!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                        aria-expanded="true" aria-controls="collapseOne">
+                        <strong>Laoban Kopitiam Wiyung</strong>
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d126636.47532793586!2d112.59536746260862!3d-7.3100251218240375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x2dd7fd565360e6d9%3A0x1bd8698e8ca3be00!2sJl.%20Raya%20Menganti%2C%20Babatan%2C%20Kec.%20Wiyung%2C%20Surabaya%2C%20Jawa%20Timur%2060227!3m2!1d-7.310006199999999!2d112.6777754!5e0!3m2!1sen!2sid!4v1715233787175!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <strong>Laoban Kopitiam Mulyosari</strong>
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d126649.16218611828!2d112.71325126214603!3d-7.265141866747921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x2dd7fb0008f1c997%3A0x93bab84f775d7292!2sJl.%20Raya%20Mulyosari%20No.166%2C%20Kalisari%2C%20Kec.%20Mulyorejo%2C%20Surabaya%2C%20Jawa%20Timur!3m2!1d-7.2651493!2d112.79565319999999!5e0!3m2!1sen!2sid!4v1715234013287!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <strong>Laoban Kopitiam Denpasar</strong>
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d126210.40275075895!2d115.14846477814376!3d-8.684409855608326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x2dd241004252ee53%3A0x8091a327d4ce5cd7!2sJl.%20Tukad%20Barito%20Timur%20No.35%2C%20Panjer%2C%20Kec.%20Denpasar%20Bar.%2C%20Kota%20Denpasar%2C%20Bali%2080225!3m2!1d-8.6844187!2d115.23086669999999!5e0!3m2!1sen!2sid!4v1715249073122!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+        <!-- ACCORDION BOOTSTRAP -->
+    </div>
+
+    <div class="col-md-8">
+        <img src="" alt="">
+
+    </div>
+
+</section>
+<!-- ***** Franchise Area Ends ***** -->
 <?php include_once __DIR__."/template/footer.php"; ?>
--- template/header.php
+++ template/header.php
@@ -11,13 +11,15 @@
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap"
         rel="stylesheet">
-        
-        
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
-        <link rel="manifest" href="assets/images/site.webmanifest">
-        <title>Laoban Nusantara</title>
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/site.webmanifest">
+    <title>Laoban Nusantara</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
 
@@ -37,9 +39,9 @@
 </head>
 
 <body>
-    
-<!-- ***** Preloader Start ***** -->
-<div id="preloader">
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
         <div class="jumper">
             <div></div>
             <div></div>
@@ -57,13 +59,16 @@
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
                         <a href="index.php" class="logo">
-                            <img src="assets/images/logo_red.png" align="klassy cafe html template" style="width: 225px; height: 80px; margin-left: -70px; ;">
+                            <img src="assets/images/logo_red.png" align="klassy cafe html template"
+                                style="width: 225px; height: 80px; margin-left: -70px; ;">
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-                            <li class="scroll-to-section"><a href="index.php" class="<?php echo ($current_page == 'index') ? 'active' : ''; ?>">Home</a></li>
-                            <li class="scroll-to-section"><a href="about.php" class="<?php echo ($current_page == 'about') ? 'active' : ''; ?>">About</a></li>
+                            <li class="scroll-to-section"><a href="index.php"
+                                    class="<?php echo ($current_page == 'index') ? 'active' : ''; ?>">Home</a></li>
+                            <li class="scroll-to-section"><a href="about.php"
+                                    class="<?php echo ($current_page == 'about') ? 'active' : ''; ?>">About</a></li>
 
                             <!-- 
                             <li class="submenu">
@@ -76,8 +81,11 @@
                             </li>
                         -->
                             <!-- <li class="scroll-to-section"><a href="menu.php">Menu</a></li> -->
-                            <li class="scroll-to-section"><a href="menu.php" class="<?php echo ($current_page == 'menu') ? 'active' : ''; ?>">Menu</a></li>
-                            <li class="scroll-to-section"><a href="ourFranchise.php" class="<?php echo ($current_page == 'franchise') ? 'active' : ''; ?>">Our Franchise</a></li>
+                            <li class="scroll-to-section"><a href="menu.php"
+                                    class="<?php echo ($current_page == 'menu') ? 'active' : ''; ?>">Menu</a></li>
+                            <li class="scroll-to-section"><a href="ourFranchise.php"
+                                    class="<?php echo ($current_page == 'franchise') ? 'active' : ''; ?>">Our
+                                    Franchise</a></li>
                             <!-- <li class="submenu">
                                 <a href="javascript:;">Features</a>
                                 <ul>
@@ -88,7 +96,9 @@
                                 </ul>
                             </li> -->
                             <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
-                            <li class="scroll-to-section"><a href="contact.php" class="<?php echo ($current_page == 'contact') ? 'active' : ''; ?>">Partnership</a></li>
+                            <li class="scroll-to-section"><a href="contact.php"
+                                    class="<?php echo ($current_page == 'contact') ? 'active' : ''; ?>">Partnership</a>
+                            </li>
                         </ul>
                         <a class='menu-trigger'>
                             <span>Menu</span>
