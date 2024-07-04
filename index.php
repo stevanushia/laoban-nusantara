d75a6b348cc24f42e281e42c728dead3f722764d
Stevanus Hia <stevanushiaa@gmail.com>
2024-07-04 23:38:18 +0700

    fixed main banner on index

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ include_once __DIR__."/template/header.php";
                 <div class="main-banner header-text">
                     <div class="Modern-Slider">
                         <?php
-                                for ($i=1; $i < 9; $i++) {  
-                                    ?>
+                            for ($i=1; $i < 9; $i++) {  
+                        ?>
                         <!-- Item -->
                         <div class="item">
                             <div class="img-fill">
@@ -34,8 +34,8 @@ include_once __DIR__."/template/header.php";
                         </div>
                         <!-- // Item -->
                         <?php
-                                }
-                            ?>
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
@@ -45,47 +45,9 @@ include_once __DIR__."/template/header.php";
 <!-- ***** Main Banner Area End ***** -->
 
 
-<!-- ***** Founder Area Starts ***** -->
 
-<!-- <section class="section" id="about">
-        <div id="chefs">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-xs-12">
-                    <div class="left-text-content">
-                        <div class="section-heading">
-                            <h6>Founder Laoban Nusantara</h6>
-                            <h2 style="margin-bottom: -45px;">ALVIN OSH</h2>
-                        </div>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus maxime, repudiandae facere ad exercitationem ea laborum nostrum libero praesentium autem expedita minima iusto atque in commodi! Incidunt nisi inventore praesentium?
-                        Sequi, architecto in excepturi et, doloremque iste, consectetur vitae tempora sit hic dicta illum qui. Ratione enim nisi delectus atque modi reprehenderit aspernatur ducimus, ut vero, quod omnis excepturi! Tempora!
-                        At vitae rem minus aut quasi placeat praesentium. Quasi fugiat recusandae aliquid ipsam, fugit molestiae provident vel soluta unde ratione incidunt dolores esse repellendus sapiente optio, nemo eum eaque vitae.
-                        </p> 
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-xs-12">
-                    <div class="right-content">
-                    <div class="chef-item">
-                        <div class="thumb">
-                            <div class="overlay"></div>
-                            <ul class="social-icons">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                            </ul>
-                            <img src="assets/images/FotoCEO.jpg" alt="Founder" style="width: 325px;" >
-                        </div>
-                        <div class="down-content">
-                            <h4>Alvin Osh</h4>
-                            <span>CEO</span>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-    </section> -->
+
+<!-- ***** Founder Area Starts ***** -->
 
 <!-- ***** Founder Area Ends ***** -->
 <section class="section" id="chefs">
